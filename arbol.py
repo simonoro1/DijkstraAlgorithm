a30d683bdc27dedab411703e281161e41e484743
Simon Oro <58258745+simonoro1@users.noreply.github.com>
2023-11-08 12:33:49 -0300

    arbol

---
+++ arbol.py
@@ -0,0 +1,19 @@
+class Arbol:
+    def __init__(self, elemento):
+        self.root = root
+
+class Nodo:
+    left = right = None
+    def __init__(self, value):
+        self.value = value
+
+
+
+def buscarSubArbol(arbol, elemento) => {
+
+    if root = None 
+    return arbol
+
+    for sub arbol in arbol.hijos:
+
+}
